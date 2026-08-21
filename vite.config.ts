800f805608d33f1c6036dce12a8488f89e185350
RenzWay <narendradikaprasoca10@gmail.com>
2026-08-21 13:49:50 +0700

    ADD: add base for pages 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://renzway.github.io/air-trash/",
+  base: "/air-trash/",
   server: {
     allowedHosts: true,
   },
